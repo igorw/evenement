43d5d461185e1f13bf7d476151c84a4d741befb7
Igor Wiedler <igor@wiedler.ch>
2011-11-09 21:00:07 +0100

    EventEmitter2 with onAny, offAny, many and tests

---
+++ src/Evenement/EventEmitter2.php
@@ -0,0 +1,86 @@
+<?php
+
+/*
+ * This file is part of Evenement.
+ *
+ * Copyright (c) 2011 Igor Wiedler
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is furnished
+ * to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+namespace Evenement;
+
+class EventEmitter2 extends EventEmitter
+{
+    protected $options;
+    protected $anyListeners = array();
+
+    public function __construct(array $options = array())
+    {
+        $this->options = array_merge(array(
+            'delimiter' => '.',
+        ), $options);
+    }
+
+    public function onAny($listener)
+    {
+        $this->anyListeners[] = $listener;
+    }
+
+    public function offAny($listener)
+    {
+        if (false !== $index = array_search($listener, $this->anyListeners, true)) {
+            unset($this->anyListeners[$index]);
+        }
+    }
+
+    public function many($event, $timesToListen, $listener)
+    {
+        $that = $this;
+
+        $timesListened = 0;
+
+        if ($timesToListen == 0) {
+            return;
+        }
+
+        if ($timesToListen < 0) {
+            throw new \OutOfRangeException('You cannot listen less than zero times.');
+        }
+
+        $manyListener = function () use ($that, &$timesListened, &$manyListener, $event, $timesToListen, $listener) {
+            if (++$timesListened == $timesToListen) {
+                $that->removeListener($event, $manyListener);
+            }
+
+            call_user_func_array($listener, func_get_args());
+        };
+
+        $this->on($event, $manyListener);
+    }
+
+    public function emit($event, array $arguments = array())
+    {
+        foreach ($this->anyListeners as $listener) {
+            call_user_func_array($listener, $arguments);
+        }
+
+        parent::emit($event, $arguments);
+    }
+}
---
+++ tests/Evenement/Tests/EventEmitter2Test.php
@@ -0,0 +1,111 @@
+<?php
+
+/*
+ * This file is part of Evenement.
+ *
+ * Copyright (c) 2011 Igor Wiedler
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is furnished
+ * to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+namespace Evenement\Tests;
+
+use Evenement\EventEmitter2;
+
+class EventEmitter2Test extends \PHPUnit_Framework_TestCase
+{
+    private $emitter;
+
+    public function setUp()
+    {
+        $this->emitter = new EventEmitter2();
+    }
+
+    public function testOnAny()
+    {
+        $this->emitter->onAny(function () {});
+    }
+
+    public function testOnAnyWithEmit()
+    {
+        $listenerCalled = 0;
+
+        $this->emitter->onAny(function () use (&$listenerCalled) {
+            $listenerCalled++;
+        });
+
+        $this->assertSame(0, $listenerCalled);
+
+        $this->emitter->emit('foo');
+
+        $this->assertSame(1, $listenerCalled);
+
+        $this->emitter->emit('bar');
+
+        $this->assertSame(2, $listenerCalled);
+    }
+
+    public function testoffAnyWithEmit()
+    {
+        $listenerCalled = 0;
+
+        $listener = function () use (&$listenerCalled) {
+            $listenerCalled++;
+        };
+
+        $this->emitter->onAny($listener);
+        $this->emitter->offAny($listener);
+
+        $this->assertSame(0, $listenerCalled);
+        $this->emitter->emit('foo');
+        $this->assertSame(0, $listenerCalled);
+    }
+
+    /**
+     * @dataProvider provideMany
+     */
+    public function testMany($amount)
+    {
+        $listenerCalled = 0;
+
+        $this->emitter->many('foo', $amount, function () use (&$listenerCalled) {
+            $listenerCalled++;
+        });
+
+        for ($i = 0; $i < $amount; $i++) {
+            $this->assertSame($i, $listenerCalled);
+            $this->emitter->emit('foo');
+        }
+
+        $this->emitter->emit('foo');
+        $this->assertSame($amount, $listenerCalled);
+    }
+
+    public function provideMany()
+    {
+        return array(
+            array(0),
+            array(1),
+            array(2),
+            array(3),
+            array(4),
+            array(400),
+        );
+    }
+}
