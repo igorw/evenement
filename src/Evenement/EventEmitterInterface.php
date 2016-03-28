58905164bc80095340c6e17aca08f3b4ab4227dd
Brian <wormling@gmail.com>
2016-03-28 20:03:03 +0300

    Allow getting all listeners

--- src/Evenement/EventEmitterInterface.php
+++ src/Evenement/EventEmitterInterface.php
@@ -17,6 +17,6 @@ interface EventEmitterInterface
     public function once($event, callable $listener);
     public function removeListener($event, callable $listener);
     public function removeAllListeners($event = null);
-    public function listeners($event);
+    public function listeners($event = null);
     public function emit($event, array $arguments = []);
 }
