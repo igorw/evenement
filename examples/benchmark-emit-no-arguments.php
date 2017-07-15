816088e9d6638f8c36990fae6ac01b8705da6ab8
Cees-Jan Kiewiet <ceesjank@gmail.com>
2017-07-16 00:36:51 +0200

    Add copyright notice to benchmarks

--- examples/benchmark-emit-no-arguments.php
+++ examples/benchmark-emit-no-arguments.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of Evenement.
+ *
+ * (c) Igor Wiedler <igor@wiedler.ch>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 const ITERATIONS = 10000000;
 
 use Evenement\EventEmitter;
